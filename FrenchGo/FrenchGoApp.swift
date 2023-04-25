d29cf1446580c127f2422f0e90be9fdec567720e
AndreiMirica <64529696+AndreiMirica19@users.noreply.github.com>
2023-04-25 11:08:05 +0300

    Initial Commit

---
+++ FrenchGo/FrenchGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchGoApp.swift
+//  FrenchGo
+//
+//  Created by Andrei Mirica on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct FrenchGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
